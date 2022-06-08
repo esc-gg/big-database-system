da8be65a54121293239a33a5f0567ceaa8cec6d2
동동이 <drdragon777@naver.com>
2022-06-09 05:06:59 +0900

    Add summaryMocks & scss color

---
+++ client/src/mock/summaryInfo.ts
@@ -0,0 +1,13 @@
+export const summaryInfoMocks = [
+  {
+    gameMode: 'CLASSIC',
+    isVictory: false,
+    gameDuration: 1420,
+    championName: 'Jayce',
+    kills: 21,
+    deaths: 12,
+    assists: 18,
+    victoryMember: ['민혜성', '돈 욘', 'liiliiiilii', '준 욘', '고대산pinhove'],
+    loseMember: ['밉상참새', 'sho1109', '태 수 멍', '갱도 간다', '규래찌'],
+  }
+];
